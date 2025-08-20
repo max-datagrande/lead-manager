b03da6e30ec087944eea4aa2e1ddf002de890c37
Max Jimenez <maxjip@gmail.com>
2025-08-20 00:01:56 -0500

    feat(config): add slack webhook and ipapi service configurations
    Add new configuration options for Slack webhook URL and IPAPI service including base URL and token. Remove deprecated Slack notifications configuration.

--- config/services.php
+++ config/services.php
@@ -28,9 +28,14 @@ return [
   ],
 
   'slack' => [
-    'notifications' => [
+    'webhook_url' => env('SLACK_WEBHOOK_URL'),
+    /*  'notifications' => [
       'bot_user_oauth_token' => env('SLACK_BOT_USER_OAUTH_TOKEN'),
       'channel' => env('SLACK_BOT_USER_DEFAULT_CHANNEL'),
-    ],
+    ], */
+  ],
+  'ipapi' => [
+    'base_url' => env('IPAPI_BASE_URL', 'https://ipapi.co'),
+    'token' => env('IPAPI_TOKEN'),
   ],
 ];
