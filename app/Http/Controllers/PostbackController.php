560ddada9d0a160127126639951f660df514d817
Max Jimenez <maxjip@gmail.com>
2025-08-22 06:19:10 -0500

    feat(Postback): add controller with basic CRUD methods
    Implement PostbackController with empty index, show and destroy methods

---
+++ app/Http/Controllers/PostbackController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Postback;
+use App\Http\Requests\StorePostbackRequest;
+use App\Http\Requests\UpdatePostbackRequest;
+
+class PostbackController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Postback $postback)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Postback $postback)
+    {
+        //
+    }
+}
