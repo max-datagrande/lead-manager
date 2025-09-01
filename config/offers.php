d2b545d763a8c5e345bba892093c695aa76769d8
Max Jimenez <maxjip@gmail.com>
2025-09-01 07:51:06 -0500

    feat(offers): add new maxconv offers for Natural Intelligence
    Add two new offer configurations for Natural Intelligence ESP and ENG with their respective tracking URLs and postback URLs

--- config/offers.php
+++ config/offers.php
@@ -4,6 +4,14 @@ return [
   'maxconv' => [
     [
       'offer_id' => '1',
+      'name' => "Global - Global - Natural Intelligence ESP",
+      'url' => "https://funnel.naturaltracking.com/v4?key=JivXHUy3cOd72PdNvFzs&pub_param_1={mc_click_id}&pub_param_2={offer_name}&external_campaign_id={campaign_id}&external_traffic_source={traffic_source_name}",
+      'postback_url' => 'https://pn10bv.mcgo2.com/conv?clid={clid}&payout={payout}&txid={txid}&currency=USD&event={event}',
+    ],
+    [
+      'offer_id' => '2',
+      'name' => "Global - Global - Natural Intelligence ENG",
+      'url' => "https://funnel.naturaltracking.com/v4?key=x6krszYYWSB3omJBs03N&pub_param_1={mc_click_id}&pub_param_2={offer_name}&external_campaign_id={campaign_id}&external_traffic_source={traffic_source_name}",
       'postback_url' => 'https://pn10bv.mcgo2.com/conv?clid={clid}&payout={payout}&txid={txid}&currency=USD&event={event}',
     ]
   ]
