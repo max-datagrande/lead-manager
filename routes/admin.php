0f435efa5cf078498dd4e8a27aaa63cf6bb495a6
Max Jimenez <maxjip@gmail.com>
2026-02-21 16:25:21 -0500

    fix(routes): correct indentation for resource routes in admin.php

--- routes/admin.php
+++ routes/admin.php
@@ -5,6 +5,6 @@ use App\Http\Controllers\Admin\WhitelistEntryController;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware(['auth', 'verified', 'admin'])->prefix('admin')->name('admin.')->group(function () {
-    Route::resource('users', UserController::class);
-    Route::resource('whitelist', WhitelistEntryController::class)->except(['show', 'create', 'edit']);
+  Route::resource('users', UserController::class)->except(['show', 'create', 'edit']);
+  Route::resource('whitelist', WhitelistEntryController::class)->except(['show', 'create', 'edit']);
 });
