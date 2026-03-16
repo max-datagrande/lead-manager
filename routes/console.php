0435c43dace3465f822b006ed715e70f94dc1e43
Max Jimenez <maxjip@gmail.com>
2026-03-16 07:36:44 -0500

    chore(routes): comment out unused job scheduling for RetryFailedPostbacksJob in console routes

--- routes/console.php
+++ routes/console.php
@@ -1,8 +1,16 @@
 <?php
 
+/* use App\Jobs\RetryFailedPostbacksJob; */
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+/* use Illuminate\Support\Facades\Schedule; */
 
 Artisan::command('inspire', function () {
   $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
+
+/* Schedule::job(new RetryFailedPostbacksJob)
+    ->everyMinute()
+    ->name('postback-retry-failed')
+    ->withoutOverlapping(5)
+    ->onOneServer(); */
