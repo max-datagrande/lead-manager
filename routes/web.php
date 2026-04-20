e11954d81a228646bbc4455ba7a512e65e7b4146
Max Jimenez <maxjip@gmail.com>
2026-04-20 13:50:27 -0500

    feat: implement ValidationRuleController and associated request classes for managing lead quality validation rules

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,7 @@ use App\Http\Controllers\OfferwallController;
 use App\Http\Controllers\PerformanceController;
 use App\Http\Controllers\LeadQuality\ProviderController as LeadQualityProviderController;
 use App\Http\Controllers\LeadQuality\ProviderTestController as LeadQualityProviderTestController;
+use App\Http\Controllers\LeadQuality\ValidationRuleController as LeadQualityValidationRuleController;
 use App\Http\Controllers\PingPost\BuyerController;
 use App\Http\Controllers\PingPost\WorkflowAlertController;
 use App\Http\Controllers\PingPost\WorkflowController;
@@ -217,6 +218,10 @@ Route::middleware(['auth', 'verified'])->group(function () {
       Route::resource('providers', LeadQualityProviderController::class)
         ->except(['show'])
         ->whereNumber('provider');
+      Route::resource('validation-rules', LeadQualityValidationRuleController::class)
+        ->except(['show'])
+        ->parameters(['validation-rules' => 'validationRule'])
+        ->whereNumber('validationRule');
     });
 });
 
